3df389cc84966d2ad414e307edb03943e22f068d
ukaganaslan <umitkaganaslan@gmail.com>
2025-04-29 00:51:55 +0300

    taskwish
    taskwish

---
+++ Parent.java
@@ -0,0 +1,19 @@
+public class Parent extends User {
+    public Parent(String userId) {
+        super(userId);
+    }
+
+    public void approveWish(Wish wish, int requiredLevel) {
+        wish.setStatus("Approved");
+        wish.setLevel(requiredLevel);
+        System.out.println("Wish " + wish.getWishName() + " approved by Parent, required level: " + requiredLevel);
+    }
+
+    public void manageTasks(Task task) {
+        System.out.println("Parent " + userId + " is managing task: " + task.getTaskName());
+    }
+
+    public void manageWish(Wish wish) {
+        System.out.println("Parent " + userId + " is managing wish: " + wish.getWishName());
+    }
+}
